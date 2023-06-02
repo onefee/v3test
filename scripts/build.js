5a7196621cffa520ab792157b235de0e2bbd9e4f
bruce <bruce@sixthnet.com>
2023-06-02 18:42:59 +0800

    build参数

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,24 @@
 'use strict';
-// require('../config/env'); vue-cli-service build &&
-
-const path = require('path');
+// const path = require('path');
 const fs = require('fs');
 
 setting();
 
 function setting() {
-  let ds = fs.readFileSync('conf.json').toString(); //, 'utf-8'
-  let dt = JSON.parse(ds);
-  dt.version += 1;
-  console.log(dt);
-  // console.log("set", path, fs);
-  fs.writeFileSync('conf.json', JSON.stringify(dt));
+  let action = '';
+  if (process.argv.length > 2) {
+    action = process.argv[2];
+  }
+  const fp = 'conf.json';
+  // 添加版本号
+  if (action === 'pre') {
+    let ds = fs.readFileSync(fp).toString(); //, 'utf-8'
+    let dt = JSON.parse(ds);
+    dt.version += 1;
+    console.log(dt);
+    // console.log('set', fs);
+    fs.writeFileSync(fp, JSON.stringify(dt));
+  } else {
+    fs.copyFileSync(fp, 'dist/conf.json');
+  }
 }
--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,13 @@ const v = require('./conf.json').version;
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
   /**
    * 修改插件的配置
    * @param config
    */
   chainWebpack: (config) => {
     config.plugin('html').tap((arg) => {
-      console.log('vers', v);
       arg[0].version = v;
       return arg;
     });
